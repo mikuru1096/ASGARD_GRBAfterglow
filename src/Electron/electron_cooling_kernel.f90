dc444eb72ce22c1fb7101e80da0f8e38ab73b0e9
jia <jia@Analyzer.localdomain>
2026-06-20 08:51:01 +0800

    Fix legacy electron cooling fast path

--- src/Electron/electron_cooling_kernel.f90
+++ src/Electron/electron_cooling_kernel.f90
@@ -227,12 +227,15 @@ end function ssa_cached_cell_segment
 
 ! 对给定种子光子场累加SSA冷却率：低频Σ ∝ ν^(5/3)，高频Σ ∝ 1/γ³。
 subroutine accumulate_ssa_for_seed(Num_gam_e,Num_nu,n_threads,gam_e,Seed_syn,V_low_idx,V_low_first,V_low_last, &
-                                   V_high_first,sigma_prefactor_low,sigma_prefactor_high,Cyclotron_nu,dot_gam_e)
+                                   V_high_first,V_seed_low,V_seed_high,V_seed_g1,V_seed_g2,V_seed_w1,V_seed_w2, &
+                                   sigma_prefactor_low,sigma_prefactor_high,Cyclotron_nu,dot_gam_e)
 !$ use omp_lib
 implicit REAL(8)(A-H,O-Z)
 integer, intent(in) :: Num_gam_e,Num_nu,n_threads
 real(8), intent(in) :: gam_e(Num_gam_e),Seed_syn(Num_nu)
 integer, intent(in) :: V_low_idx(Num_gam_e),V_low_first(Num_gam_e),V_low_last(Num_gam_e),V_high_first(Num_gam_e)
+real(8), intent(in) :: V_seed_low(Num_nu-1),V_seed_high(Num_nu-1),V_seed_g1(Num_nu-1),V_seed_g2(Num_nu-1)
+real(8), intent(in) :: V_seed_w1(Num_nu-1),V_seed_w2(Num_nu-1)
 real(8), intent(in) :: sigma_prefactor_low(Num_gam_e),sigma_prefactor_high(Num_gam_e),Cyclotron_nu
 real(8), intent(out) :: dot_gam_e(Num_gam_e)
 integer, parameter :: parallel_work_threshold=512
@@ -273,11 +276,11 @@ real(8) :: gam,gam2,V_lowlim,V_uplim,ssa_sum,cell_low,cell_high
     ssa_sum=zero
 
     do I_nu=V_low_first(I_gam_e),V_low_last(I_gam_e)
-       cell_low=max(ssa_seed_v_low(I_nu),V_lowlim)
-       cell_high=min(ssa_seed_v_high(I_nu),V_uplim)
+       cell_low=max(V_seed_low(I_nu),V_lowlim)
+       cell_high=min(V_seed_high(I_nu),V_uplim)
        if (cell_high > cell_low) then
-          if (cell_low == ssa_seed_v_low(I_nu) .and. cell_high == ssa_seed_v_high(I_nu)) then
-              ssa_sum=ssa_sum+ssa_cached_cell_segment(I_nu,Seed_syn(I_nu),Seed_syn(I_nu+1), &
+          if (cell_low == V_seed_low(I_nu) .and. cell_high == V_seed_high(I_nu)) then
+              ssa_sum=ssa_sum+cached_ssa_cell_segment(I_nu,Seed_syn(I_nu),Seed_syn(I_nu+1), &
                                                       sigma_prefactor_low(I_gam_e),1,Cyclotron_nu,V_uplim)
           else
               ssa_sum=ssa_sum+electron_ssa_segment(cell_low,cell_high,Seed_syn(I_nu),Seed_syn(I_nu+1), &
@@ -287,11 +290,11 @@ real(8) :: gam,gam2,V_lowlim,V_uplim,ssa_sum,cell_low,cell_high
     end do
 
     do I_nu=V_high_first(I_gam_e),Num_nu-1
-       cell_low=max(ssa_seed_v_low(I_nu),V_uplim)
-       cell_high=ssa_seed_v_high(I_nu)
+       cell_low=max(V_seed_low(I_nu),V_uplim)
+       cell_high=V_seed_high(I_nu)
        if (cell_high > cell_low) then
-          if (cell_low == ssa_seed_v_low(I_nu)) then
-              ssa_sum=ssa_sum+ssa_cached_cell_segment(I_nu,Seed_syn(I_nu),Seed_syn(I_nu+1), &
+          if (cell_low == V_seed_low(I_nu)) then
+              ssa_sum=ssa_sum+cached_ssa_cell_segment(I_nu,Seed_syn(I_nu),Seed_syn(I_nu+1), &
                                                       sigma_prefactor_high(I_gam_e),2,Cyclotron_nu,V_uplim)
           else
               ssa_sum=ssa_sum+electron_ssa_segment(cell_low,cell_high,Seed_syn(I_nu),Seed_syn(I_nu+1), &
@@ -302,6 +305,33 @@ real(8) :: gam,gam2,V_lowlim,V_uplim,ssa_sum,cell_low,cell_high
 
     dot_val=ssa_sum
 end subroutine accumulate_ssa_single_gamma
+
+real(8) function cached_ssa_cell_segment(I_nu,seed0,seed1,sigma_prefactor,mode,Cyclotron_nu,V_uplim)
+implicit REAL(8)(A-H,O-Z)
+integer, intent(in) :: I_nu,mode
+real(8), intent(in) :: seed0,seed1,sigma_prefactor,Cyclotron_nu,V_uplim
+real(8) :: seed_loc,sigma_loc,vg,wg
+
+    vg=V_seed_g1(I_nu)
+    wg=V_seed_w1(I_nu)
+    seed_loc=electron_powerlaw_interp(V_seed_low(I_nu),V_seed_high(I_nu),seed0,seed1,vg)
+    if (mode == 1) then
+        sigma_loc=sigma_prefactor*vg**(-5d0/3d0)
+    else
+        sigma_loc=sigma_prefactor*(Cyclotron_nu/vg)*dexp(-vg/V_uplim)
+    end if
+    cached_ssa_cell_segment=wg*sigma_loc*seed_loc*para_h*vg*para_c
+
+    vg=V_seed_g2(I_nu)
+    wg=V_seed_w2(I_nu)
+    seed_loc=electron_powerlaw_interp(V_seed_low(I_nu),V_seed_high(I_nu),seed0,seed1,vg)
+    if (mode == 1) then
+        sigma_loc=sigma_prefactor*vg**(-5d0/3d0)
+    else
+        sigma_loc=sigma_prefactor*(Cyclotron_nu/vg)*dexp(-vg/V_uplim)
+    end if
+    cached_ssa_cell_segment=cached_ssa_cell_segment+wg*sigma_loc*seed_loc*para_h*vg*para_c
+end function cached_ssa_cell_segment
 end subroutine accumulate_ssa_for_seed
 
 ! 计算同步自吸收（SSA）冷却率 dγ/dt：构建几何→累加低频+高频贡献。
@@ -318,8 +348,9 @@ real(8) :: Cyclotron_nu
     call build_ssa_geometry(DB,Num_gam_e,Num_nu,gam_e,ssa_low_idx_cache,ssa_low_first_cache,ssa_low_last_cache, &
                             ssa_high_first_cache, &
                             ssa_prefactor_low_cache,ssa_prefactor_high_cache,Cyclotron_nu)
-    call accumulate_ssa_for_seed(Num_gam_e,Num_nu,1,gam_e,Seed_syn,ssa_low_idx_cache,ssa_low_first_cache, &
-                                 ssa_low_last_cache,ssa_high_first_cache, &
+    call accumulate_ssa_for_seed(Num_gam_e,Num_nu,n_threads,gam_e,Seed_syn,ssa_low_idx_cache,ssa_low_first_cache, &
+                                 ssa_low_last_cache,ssa_high_first_cache,ssa_seed_v_low,ssa_seed_v_high, &
+                                 ssa_seed_vg1,ssa_seed_vg2,ssa_seed_wg1,ssa_seed_wg2, &
                                  ssa_prefactor_low_cache,ssa_prefactor_high_cache,Cyclotron_nu,dot_gam_e)
 
 end subroutine electron_cooling_ssa_loss
@@ -762,7 +793,7 @@ integer :: I_chi
     end select
 end subroutine prepare_forward_cooling_aux_batch
 
-! 组装正向激波冷却率 dγ/dR：SSA + 同步 + IC/Compton Y，分离SSA和IC计算。
+! 组装正向激波冷却率 dγ/dR：index_Y=0为纯同步冷却，IC/Compton分支加入SSA回热。
 subroutine assemble_forward_cooling_split(index_Y,Epsilon_e,Epsilon_b,p,DB,Gam_e_m,Gam_e_c,Gam_e_max,R_loc,R_Gamma_loc, &
                                           beta_Gam,dNe,Num_gam_e,Num_nu,n_threads,gam_e,V_seed, &
                                           Seed_syn_ssa,cooling_aux,dEl)
@@ -774,7 +805,11 @@ real(8), intent(in) :: gam_e(Num_gam_e),V_seed(Num_nu),Seed_syn_ssa(Num_nu),cool
 real(8), intent(out) :: dEl(Num_gam_e)
 real(8) :: Compton(Num_gam_e),dot_gam_e_SSA(Num_gam_e)
 
-    call electron_cooling_ssa_loss(DB,Num_gam_e,Num_nu,n_threads,gam_e,V_seed,Seed_syn_ssa,dot_gam_e_SSA)
+    if (index_Y == 0) then
+        dot_gam_e_SSA=zero
+    else
+        call electron_cooling_ssa_loss(DB,Num_gam_e,Num_nu,n_threads,gam_e,V_seed,Seed_syn_ssa,dot_gam_e_SSA)
+    end if
     call assemble_forward_cooling_from_terms(index_Y,Epsilon_e,Epsilon_b,p,DB,Gam_e_m,Gam_e_c,Gam_e_max,R_loc,R_Gamma_loc, &
                                              beta_Gam,dNe,Num_gam_e,gam_e,Compton,dot_gam_e_SSA,cooling_aux,dEl)
 end subroutine assemble_forward_cooling_split
@@ -792,7 +827,11 @@ real(8), intent(out) :: dEl(Num_gam_e,Num_chi)
 real(8) :: Compton(Num_gam_e),Gam_e_max_cell,dot_gam_e_SSA(Num_gam_e,Num_chi)
 integer :: I_chi
 
-    call electron_cooling_ssa_loss_batch(DB,Num_gam_e,Num_nu,Num_chi,n_threads,gam_e,V_seed,Seed_syn_ssa,dot_gam_e_SSA)
+    if (index_Y == 0) then
+        dot_gam_e_SSA=zero
+    else
+        call electron_cooling_ssa_loss_batch(DB,Num_gam_e,Num_nu,Num_chi,n_threads,gam_e,V_seed,Seed_syn_ssa,dot_gam_e_SSA)
+    end if
     do I_chi=1,Num_chi
        Gam_e_max_cell=Gam_e_max
        call assemble_forward_cooling_from_terms(index_Y,Epsilon_e,Epsilon_b,p,DB,Gam_e_m,Gam_e_c,Gam_e_max_cell,R_loc,R_Gamma_loc, &
@@ -819,7 +858,7 @@ real(8), intent(out) :: dEl(Num_gam_e)
 
     select case(index_Y)
     case(0)
-        dEl=(f_r-dot_gam_e_SSA*ssa_scale)*gam_e
+        dEl=f_r*gam_e
     case(1)
         dEl=(f_r+(cooling_aux-dot_gam_e_SSA)*ssa_scale)*gam_e
     case(2)
@@ -838,7 +877,7 @@ real(8), intent(out) :: dEl(Num_gam_e)
     end select
 end subroutine assemble_forward_cooling_from_terms
 
-! 正向激波冷却主入口：准备IC辅助量→计算SSA→组装冷却率 dγ/dt。
+! 正向激波冷却主入口：准备IC辅助量，按index_Y选择是否计算SSA回热，再组装冷却率。
 subroutine get_forward_cooling(index_Y,Epsilon_e,Epsilon_b,p,DB,Gam_e_m,Gam_e_c,Gam_e_max,R_loc,R_Gamma_loc, &
                                beta_Gam,dNe,Num_gam_e,Num_nu,n_threads,gam_e,V_seed,P_syn,Seed_syn,dEl)
 implicit REAL(8)(A-H,O-Z)
@@ -851,7 +890,11 @@ real(8) :: cooling_aux(Num_gam_e)
 real(8) :: Compton(Num_gam_e),dot_gam_e_SSA(Num_gam_e)
 
     call prepare_forward_cooling_aux(index_Y,Num_gam_e,Num_nu,n_threads,gam_e,V_seed,P_syn,Seed_syn,cooling_aux)
-    call electron_cooling_ssa_loss(DB,Num_gam_e,Num_nu,n_threads,gam_e,V_seed,Seed_syn,dot_gam_e_SSA)
+    if (index_Y == 0) then
+        dot_gam_e_SSA=zero
+    else
+        call electron_cooling_ssa_loss(DB,Num_gam_e,Num_nu,n_threads,gam_e,V_seed,Seed_syn,dot_gam_e_SSA)
+    end if
     call assemble_forward_cooling_from_terms(index_Y,Epsilon_e,Epsilon_b,p,DB,Gam_e_m,Gam_e_c,Gam_e_max,R_loc,R_Gamma_loc, &
                                              beta_Gam,dNe,Num_gam_e,gam_e,Compton,dot_gam_e_SSA,cooling_aux,dEl)
 end subroutine get_forward_cooling
